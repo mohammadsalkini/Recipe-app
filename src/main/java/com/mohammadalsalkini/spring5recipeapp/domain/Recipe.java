d98195f9ed93ca86c68712ff1fd152c5e199e914
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-03 21:47:58 +0200

    add Recipe class

--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -2,8 +2,21 @@ package com.mohammadalsalkini.spring5recipeapp.domain;
 
 /**
  * @author Mohammad Alsalkini
- * @project spring5-recipe-app
+ * @project recipe-app
  * @created 03.04.2020 - 21:35
  */
 public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private Integer source;
+    private String url;
+    private String directions;
+
+    //todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
+    private  Notes notes;
 }
