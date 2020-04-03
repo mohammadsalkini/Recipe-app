131eb02dd101e5781d324287f89646606e949215
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-03 21:41:50 +0200

    add initial commit

---
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.mohammadalsalkini.spring5recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Mohammad Alsalkini
+ * @project spring5-recipe-app
+ * @created 03.04.2020 - 18:03
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndex() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -0,0 +1,9 @@
+package com.mohammadalsalkini.spring5recipeapp.domain;
+
+/**
+ * @author Mohammad Alsalkini
+ * @project spring5-recipe-app
+ * @created 03.04.2020 - 21:35
+ */
+public class Recipe {
+}
