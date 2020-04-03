41d8d401519e51940aba9a9bc19b2400ca7f7c9c
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-03 22:44:19 +0200

    add Difficulty Enum

---
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,11 @@
+package com.mohammadalsalkini.spring5recipeapp.domain;
+
+/**
+ * @author Mohammad Alsalkini
+ * @project Recipe-app
+ * @created 03.04.2020 - 22:40
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -24,8 +24,6 @@ public class Recipe {
     private String url;
     private String directions;
 
-    //todo add
-    //private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -34,6 +32,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private  Notes notes;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -113,5 +114,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
