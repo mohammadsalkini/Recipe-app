b8ab4a0fb9a3a956ad2929e991ce150f9843693a
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-04 00:51:50 +0200

    add bootstrap Data

--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Ingredient.java
@@ -15,15 +15,24 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
     @ManyToOne
     private Recipe recipe;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private UnitOfMeasure unitOfMeasure;
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = unitOfMeasure;
+        this.recipe = recipe;
+
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.mohammadalsalkini.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -22,10 +23,12 @@ public class Recipe {
     private Integer servings;
     private Integer source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -40,7 +43,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    Set<Category> categories;
+    Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
