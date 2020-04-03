7a71ff9446f1f1803fee84fe86a6df837ff4875e
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-03 22:30:48 +0200

    add Ingredient Entity

--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.mohammadalsalkini.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * @author Mohammad Alsalkini
@@ -25,6 +26,8 @@ public class Recipe {
 
     //todo add
     //private Difficulty difficulty;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
