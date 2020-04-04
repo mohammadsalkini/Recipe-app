e837280ce9a712a836884f6a5dd9e95267fa955c
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-04 11:03:58 +0200

    add new update

--- src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/domain/Recipe.java
@@ -148,4 +148,10 @@ public class Recipe {
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
+
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
 }
