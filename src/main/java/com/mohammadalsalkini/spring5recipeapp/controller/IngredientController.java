59ff1104dc8159cdd6dea40f17a99d9e6008fafd
mohammadsalkini <eng.salkini@hotmail.com>
2020-04-05 11:21:53 +0200

    add view Ingredients

---
+++ src/main/java/com/mohammadalsalkini/spring5recipeapp/controller/IngredientController.java
@@ -0,0 +1,33 @@
+package com.mohammadalsalkini.spring5recipeapp.controller;
+
+import com.mohammadalsalkini.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author Mohammad Alsalkini
+ * @project Recipe-app
+ * @created 05.04.2020 - 11:09
+ */
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredients")
+    public String ListIngredients(@PathVariable String recipeId, Model model) {
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}
